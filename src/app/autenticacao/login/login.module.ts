512a035f91afa736262f275ddb3eb4194b85334d
Igor Meira <meira.igor@gmail.com>
2019-05-31 23:10:13 -0300

    criando model login

--- src/app/autenticacao/login/login.module.ts
+++ src/app/autenticacao/login/login.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatListModule,
+  MatTooltipModule,
+  MatIconModule,
+  MatSnackBarModule
+} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { LoginComponent, LogarComponent } from './components';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule
   ],
   declarations: [
-  	LoginComponent,
-  	LogarComponent
+    LoginComponent,
+    LogarComponent
   ]
 })
 export class LoginModule { }
